d5836813946b6f95f81fad5fe1235eb1fe19541f
Олег <@OLEG-PC>
2016-10-12 17:35:42 +0300

    My first commit uiii(update Server)

--- GasProject Server/Server.cpp
+++ GasProject Server/Server.cpp
@@ -1,9 +1,19 @@
 #include <SFML/Network.hpp>
+#include "World.hpp"
 
 class Server {
-
+public:
+	Server();
 };
 
+Server::Server()
+{
+	World world;
+	world.FillingWorld();	
+}
+
 int main() {
+	Server LoH;
+
 	return 0;
 }
