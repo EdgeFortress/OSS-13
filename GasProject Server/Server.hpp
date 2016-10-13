985103752acca98c020dd0e3bbe55e08b17aa66a
Insineer <umrikhinal@gmail.com>
2016-10-14 01:27:01 +0300

    Class Game added

---
+++ GasProject Server/Server.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <list>
+#include <memory>
+
+template<class T>
+using uptr = std::unique_ptr<T>;
+
+class Server;
+class World;
+
+class Game {
+private:
+	Server *server;
+	uptr<World> world;
+
+public:
+	Game(Server *server);
+};
+
+class Server {
+private:
+	std::list<uptr<Game>> games;
+
+public:
+	Server();
+};
