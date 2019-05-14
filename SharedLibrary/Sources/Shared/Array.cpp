5537ed24b47e8c3ff5a678f6d204888fc0a9aae9
sunail <sunail@users.noreply.github.com>
2019-05-14 18:20:40 +0300

    fix(array): add missing const

--- SharedLibrary/Sources/Shared/Array.cpp
+++ SharedLibrary/Sources/Shared/Array.cpp
@@ -1,10 +1,10 @@
 #include "Array.hpp"
 
 namespace uf {
-	uint flat_index (vec3u xyz, uint w, uint h) {
+	uint flat_index (const vec3u xyz, uint w, uint h) {
 		return xyz.z * h * w + xyz.y * w + xyz.x;
 	}
-	uint flat_index (vec2u xy, uint w) {
+	uint flat_index (const vec2u xy, uint w) {
 		return xy.y * w + xy.x;
 	}
 }
