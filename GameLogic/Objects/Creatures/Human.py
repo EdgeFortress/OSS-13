bc329ffcc75adb7f79f77eca207fbd0622e46b1c
Insineer <insineer@yandex.com>
2019-09-21 23:12:42 +0300

    [GL] feat(uniform): add more uniforms

--- GameLogic/Objects/Creatures/Human.py
+++ GameLogic/Objects/Creatures/Human.py
@@ -13,7 +13,7 @@ class Human(Creature):
 
 	def __init__(self):
 		super().__init__()
-		self.PutOn(CreateObject("Objects.Items.Clothes.Uniform.Uniform"))
+		self.PutOn(CreateObject("Objects.Items.Clothes.Uniform.Grey"))
 		self.__updateActiveHandIcon()
 		self.AddVerb("drop", lambda player: self.Drop())
 
--- GameLogic/Objects/Items/Clothes/Uniform.py
+++ GameLogic/Objects/Items/Clothes/Uniform.py
@@ -1,9 +1,30 @@
 from Objects.Items.Clothing import Clothing, MobSlot
 
 class Uniform(Clothing):
-	defName = "Uniform"
-	defSprite = "grey"
-
 	def __init__(self):
 		super().__init__()
 		self.slot = MobSlot.UNIFORM
+
+class Grey(Uniform):
+	defName = "Grey uniform"
+	defSprite = "grey"
+
+class Captain(Uniform):
+	defName = "Captain uniform"
+	defSprite = "captain"
+
+class Atmospherics(Uniform):
+	defName = "Atmospherics uniform"
+	defSprite = "atmospherics"
+
+class Security(Uniform):
+	defName = "Security uniform"
+	defSprite = "security"
+
+class Medic(Uniform):
+	defName = "Medic uniform"
+	defSprite = "medic"
+
+class Clown(Uniform):
+	defName = "Clown uniform"
+	defSprite = "clown"
