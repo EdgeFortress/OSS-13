2317d6fa418b2d39d9e16bd2fa2352bce9fb4e50
Insineer <insineer@yandex.com>
2019-09-29 22:30:03 +0300

    [CLT] refact(Sprite): make it possible to rotate sprite with pure directions
    + refactor according to common codestyle

--- OSS13 Client/Sources/Graphics/SpriteFactory.cpp
+++ OSS13 Client/Sources/Graphics/SpriteFactory.cpp
@@ -3,55 +3,15 @@
 #include <Client.hpp>
 
 #include <Shared/Global.hpp>
+#include <Shared/ErrorHandling.h>
 
 Sprite SpriteFactory::CreateSprite(uint32_t id) {
-	if (!id--) // should be from 1 to Global::MAX_SPRITE_ID! Decrease it to use as index
-		throw std::exception(); // "ID is null!"
+	EXPECT_WITH_MSG(id--, "ID is null!");
 
 	auto state = static_cast<Global::ItemSpriteState>(id & uint32_t(Global::ItemSpriteState::MASK));
 	id -= uint32_t(state);
 	
-	if (id < recipes.size()) {
-		Sprite sprite;
+	EXPECT_WITH_MSG(id < recipes.size(), "ID miss!");
 
-		sprite.key = recipes[id].title;
-
-		switch (state) {
-			case Global::ItemSpriteState::DEFAULT: {
-				sprite.texture = recipes[id].texture;
-				sprite.firstFrame = recipes[id].firstFrame;
-				sprite.frames = recipes[id].frames;
-				sprite.frameTime = recipes[id].frameTime;
-				sprite.directed = recipes[id].directed;
-				break;
-			};
-			case Global::ItemSpriteState::ON_MOB: {
-				sprite.texture = recipes[id].mobState_texture;
-				sprite.firstFrame = recipes[id].mobState_firstFrame;
-				sprite.directed = true;
-				break;
-			}
-			case Global::ItemSpriteState::IN_HAND_LEFT:
-			{
-				sprite.texture = recipes[id].lhandState_texture;
-				sprite.firstFrame = recipes[id].lhandState_firstFrame;
-				sprite.directed = true;
-				break;
-			}
-			case Global::ItemSpriteState::IN_HAND_RIGHT:
-			{
-				sprite.texture = recipes[id].rhandState_texture;
-				sprite.firstFrame = recipes[id].rhandState_firstFrame;
-				sprite.directed = true;
-				break;
-			}
-			default:
-				throw std::exception(); // "Sprite state is unknown!"
-		}
-
-		sprite.updateSpriteVariables();
-		return sprite;
-	}
-
-	throw std::exception(); // "ID miss!"
+	return Sprite(recipes[id], state);
 }
--- OSS13 Client/Sources/Graphics/SpriteInfo.h
+++ OSS13 Client/Sources/Graphics/SpriteInfo.h
@@ -11,17 +11,18 @@ struct SpriteInfo
 	std::string title;
 
 	const Texture *texture;
-	uint32_t firstFrame;
-	uint32_t frames;
+	uint32_t firstFrame{};
+	uint32_t frames{};
 	sf::Time frameTime;
-	bool directed;
+	bool directed{};
+	bool pureDirections{};
 
-	const Texture *mobState_texture;
-	uint32_t mobState_firstFrame;
+	const Texture *mobState_texture{};
+	uint32_t mobState_firstFrame{};
 
-	const Texture *lhandState_texture;
-	uint32_t lhandState_firstFrame;
+	const Texture *lhandState_texture{};
+	uint32_t lhandState_firstFrame{};
 
-	const Texture *rhandState_texture;
-	uint32_t rhandState_firstFrame;
+	const Texture *rhandState_texture{};
+	uint32_t rhandState_firstFrame{};
 };
