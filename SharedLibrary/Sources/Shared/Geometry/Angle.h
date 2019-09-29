ad9269963179e2d83bfe50b7bef75f9855cd6d4c
Insineer <insineer@yandex.com>
2019-09-29 22:20:50 +0300

    feat(Angle): add arithmetics and DirectionToAngle function

--- SharedLibrary/Sources/Shared/Geometry/Angle.h
+++ SharedLibrary/Sources/Shared/Geometry/Angle.h
@@ -9,11 +9,21 @@ public:
 	static Angle Radians(double radians) { return Angle(radians); }
 	static Angle Degrees(double degrees) { return Angle(uf::detail::DegressToRadians(degrees)); }
 
-	double GetRadians() { return radians; }
-	double GetDegrees() { return uf::detail::RadiansToDegrees(radians); }
+	double GetRadians() const { return radians; }
+	double GetDegrees() const { return uf::detail::RadiansToDegrees(radians); }
 
-	double Cos() { return std::cos(radians); }
-	double Sin() { return std::sin(radians); }
+	double Cos() const { return std::cos(radians); }
+	double Sin() const { return std::sin(radians); }
+
+	Angle operator+=(const Angle &other) { radians += other.radians; return *this; }
+	Angle operator-=(const Angle &other) { radians -= other.radians; return *this; }
+
+	bool operator==(const Angle &other) const { return radians == other.radians; }
+	bool operator!=(const Angle &other) const { return radians != other.radians; }
+	bool operator<=(const Angle &other) const { return radians <= other.radians; }
+	bool operator<(const Angle &other) const { return radians < other.radians; }
+	bool operator>=(const Angle &other) const { return radians >= other.radians; }
+	bool operator>(const Angle &other) const { return radians > other.radians; }
 
 private:
 	explicit Angle(double radians) : 
@@ -22,5 +32,10 @@ private:
 
 	double radians;
 };
+
+inline auto operator-(const Angle& angle) { return Angle::Radians(-angle.GetRadians()); }
+
+inline auto operator+(const Angle& left, const Angle& right) { return Angle::Radians(left.GetRadians() + right.GetRadians()); }
+inline auto operator-(const Angle& left, const Angle& right) { return Angle::Radians(left.GetRadians() - right.GetRadians()); }
 
 }
--- SharedLibrary/Sources/Shared/Global.hpp
+++ SharedLibrary/Sources/Shared/Global.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cstdint>
+
 namespace Global {
 
 const int PORT = 55700;
