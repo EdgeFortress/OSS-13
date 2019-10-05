46a6a380d53b953b8a341ffad43fdb23494a89d3
Insineer <insineer@yandex.com>
2019-10-06 00:08:33 +0300

    feat(shared tests): init plog

--- SharedLibrary/Tests/Sources/main.cpp
+++ SharedLibrary/Tests/Sources/main.cpp
@@ -1,6 +1,13 @@
 #include <gtest/gtest.h>
 
+#include <plog/Log.h>
+#include <plog/Appenders/ConsoleAppender.h>
+#include <plog/Formatters/MessageOnlyFormatter.h>
+
 int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+	plog::ConsoleAppender<plog::MessageOnlyFormatter> appender;
+	plog::init(plog::verbose, &appender);
+
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
