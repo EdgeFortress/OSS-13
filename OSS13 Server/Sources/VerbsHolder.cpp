4631e974eb5332fa4f5de50dbb07c4cc3c547c3d
Insineer <insineer@yandex.com>
2019-10-30 07:50:26 +0300

    [SRV] fix(VerbsHolder): stop breaking server when wrong verb is received

--- OSS13 Server/Sources/VerbsHolder.cpp
+++ OSS13 Server/Sources/VerbsHolder.cpp
@@ -11,7 +11,7 @@ void VerbsHolder::AddVerb(const std::string &name, Verb action) {
 void VerbsHolder::CallVerb(Player *player, const std::string &name) const {
 	try {
 		auto nameAndVerb = verbs.find(name);
-		EXPECT_WITH_MSG(nameAndVerb != verbs.end(), "Verb \""s + name + "\" doesn't exist!");
+		CHECK_WITH_MSG(nameAndVerb != verbs.end(), "Verb \""s + name + "\" doesn't exist!");
 		nameAndVerb->second(player);
 	} catch (const std::exception &e) {
 		MANAGE_EXCEPTION(e);
