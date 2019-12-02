c0a5f45a6f6b5c07ae2d9aba51100376c5767776
Insineer <insineer@yandex.com>
2019-12-02 05:14:36 +0300

    add(CI): add lemon library to CI

--- CI/travis/install_dependencies.sh
+++ CI/travis/install_dependencies.sh
@@ -21,3 +21,13 @@ if [ ! -d /home/travis/vcpkg/.git ]; then
     ./vcpkg integrate install
     ./vcpkg install gtest
 fi
+
+cd ~
+wget http://lemon.cs.elte.hu/pub/sources/lemon-1.3.1.tar.gz
+tar zxf lemon-1.3.1.tar.gz
+cd lemon-1.3.1
+mkdir build
+cd build
+cmake ..
+make
+sudo make install
